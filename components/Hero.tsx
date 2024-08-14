1e420371248db8f0ccafcab61b1ca8d3b68b15ce
jusbuckingham <jus.buckingham@gmail.com>
2024-08-14 08:52:24 -0700

    adding a mission statement

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       <div className="flex flex-col items-center">
         <h2 className="text-5xl mb-4">Welcome to Kofa.ai</h2>
         <p className="text-xl mb-8">
-          Empowering informed decision-making with advanced AI solutions. Introducing &apos;woke&apos; AI for a better future.
+          Kofa harnesses modern technology to infuse consciousness with practical solutions. We develop tools that prioritize relevant information, ensure safety, and guide informed decisions. Our mission is to create life-saving, educational, and protective technologies that address often overlooked challenges, equipping technology with the insight to serve the greater good.
         </p>
         <div className="relative w-full max-w-md">
           <Image
